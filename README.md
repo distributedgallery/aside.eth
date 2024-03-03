214d2d590bec079b9ce4818696035d1f80e0312e
Olivier Sarrouy <me@osarrouy.bzh>
2024-03-03 17:07:33 +0100

    Modularize deployment script

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # aside·eth
 
 Documentation coming soon.
+
+## Usage
+
+### Compile
+
+```bash
+forge compile
+```
+
+### Test
+
+```bash
+FOUNDRY_PROFILE=lite forge test
+```
---
+++ script/Aside0x01.deploy.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import {Aside0x01} from "../src/Aside0x01.sol";
+
+contract DeployAside0x01 is Script {
+    function run(
+        string memory baseUri,
+        address admin,
+        address minter,
+        address unlocker,
+        uint256 timelock,
+        address router,
+        bytes32 donId,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit,
+        string memory source
+    ) external {
+        vm.startBroadcast();
+        new Aside0x01(baseUri, admin, minter, unlocker, timelock, router, donId, subscriptionId, callbackGasLimit, source);
+        vm.stopBroadcast();
+    }
+}
