330406c455c97c6b54dbe1d4761600bd48283a5a
Olivier Sarrouy <osarrouy@protonmail.com>
2025-04-28 10:57:23 +0200

    Add code comments

--- src/AsideTokenManager.sol
+++ src/AsideTokenManager.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.10;
 import {ERC721GeneralSequence} from "hl-evm-contracts/erc721/ERC721GeneralSequence.sol";
 import {ITokenManager} from "hl-evm-contracts/tokenManager/interfaces/ITokenManager.sol";
 import {IPostTransfer} from "hl-evm-contracts/tokenManager/interfaces/IPostTransfer.sol";
-import {Ownable} from "hl-evm-contracts//utils/Ownable.sol";
 import {InterfaceSupportTokenManager} from "hl-evm-contracts/tokenManager/InterfaceSupportTokenManager.sol";
+import {Ownable} from "hl-evm-contracts/utils/Ownable.sol";
 
 contract AsideTokenManager is
     ITokenManager,
@@ -18,6 +18,30 @@ contract AsideTokenManager is
     mapping(uint256 => bool) private _isUnlocked;
 
     constructor() Ownable() {}
+
+    /**
+     * @notice Lock token `id`.
+     * @param id The id of the token to lock.
+     */
+    function lock(uint256 id) external onlyOwner {
+        _isUnlocked[id] = false;
+    }
+
+    /**
+     * @notice Unlock token `id`.
+     * @param id The id of the token to unlock.
+     */
+    function unlock(uint256 id) external onlyOwner {
+        _isUnlocked[id] = true;
+    }
+
+    /**
+     * @notice Returns whether token `id` is unlocked or not.
+     * @param id The id of the tocken to check whether it is unlocked or not.
+     */
+    function isUnlocked(uint256 id) public view returns (bool) {
+        return _isUnlocked[id];
+    }
 
     /**
      * @notice See {ITokenManager-canUpdateMetadata}
@@ -74,18 +98,6 @@ contract AsideTokenManager is
         /* to */ uint256 id
     ) external view override {
         if (!_isUnlocked[id]) revert TokenLocked();
-    }
-
-    function lock(uint256 tokenId) external onlyOwner {
-        _isUnlocked[tokenId] = false;
-    }
-
-    function unlock(uint256 tokenId) external onlyOwner {
-        _isUnlocked[tokenId] = true;
-    }
-
-    function isUnlocked(uint256 tokenId) public view returns (bool) {
-        return _isUnlocked[tokenId];
     }
 
     /**
